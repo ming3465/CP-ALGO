a1921f64c4687a46c2ad55b2bf6f1c25b7fa893d
Ming <78863917+ming3465@users.noreply.github.com>
2026-03-06 00:14:44 +0800

    Add template

---
+++ template2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define int long long
+
+using namespace std;
+  
+void local(){
+    #ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr); 
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    #endif
+}
+
+void solve(){
+     
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    local();
+    int tt = 1;
+    cin >> tt;
+    while(tt--) solve();
+}
